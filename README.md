357e1d55348b24e712e47bbd668e584ad5fd504c
Yarosh Anton <on.yaroshanton@gmail.com>
2025-01-16 03:50:46 +0200

    added description on readme.md

--- README.md
+++ README.md
@@ -1,70 +1,13 @@
-# Getting Started with Create React App
+Hi!
+I used a regular basic table + react + scss for styling (spent about 4-5 hours).
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Often, I use libraries like Material or Ant Design, they are perfectly customizable and manageable.
 
-## Available Scripts
+As an option, I can rewrite to one of the ready-made options + add TypeScript.
 
-In the project directory, you can run:
+I can also provide variants of tables from already realized my commercial projects.
 
-### `npm start`
+About pictures, I also implemented their loading on different screen displays, although in the table (on figma) they do not change.
+The only thing I want to clarify about helpers.jsx, I left the jsx in the file, although I know that it is better to split it into several sub-components.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
-
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+hosting: https://artworkslistview.netlify.app/
--- src/pages/Main/components/ArtworksList/index.jsx
+++ src/pages/Main/components/ArtworksList/index.jsx
@@ -1,124 +1,131 @@
-import React, {useEffect, useState} from 'react';
+import React, { Fragment, useEffect, useState } from "react";
 // helpers
-import dayjs from 'dayjs';
-import classNames from 'classnames';
+import dayjs from "dayjs";
+import classNames from "classnames";
 import {
-	renderDocuments,
-	renderPictureSources,
-	renderSortIcon,
-	renderStatus,
-	renderVisibility,
-	sortItems,
-} from './helpers';
+  renderDocuments,
+  renderPictureSources,
+  renderSortIcon,
+  renderStatus,
+  renderVisibility,
+  sortItems,
+} from "./helpers";
 // data
-import artworksData from '../../../../api_data.json';
+import artworksData from "../../../../api_data.json";
 // utils
-import {ASC, DESC} from 'utils/constants';
+import { ASC, DESC } from "utils/constants";
 // styles
-import styles from './ArtworksList.module.scss';
+import styles from "./ArtworksList.module.scss";
 
 const ArtworksList = () => {
-	const [artworks, setArtworks] = useState([]);
-	const [sortField, setSortField] = useState('');
-	const [sortDirection, setSortDirection] = useState(DESC);
+  const [artworks, setArtworks] = useState([]);
+  const [sortField, setSortField] = useState("");
+  const [sortDirection, setSortDirection] = useState(DESC);
 
-	useEffect(() => {
-		// TODO: add async function to fetch data from the server
-		setArtworks(artworksData.items);
-	}, []);
+  useEffect(() => {
+    // TODO: add async function to fetch data from the server
+    setArtworks(artworksData.items);
+  }, []);
 
-	const handleSort = (field) => {
-		const direction = sortField === field && sortDirection === DESC ? ASC : DESC;
-		setSortField(field);
-		setSortDirection(direction);
+  const handleSort = (field) => {
+    const direction =
+      sortField === field && sortDirection === DESC ? ASC : DESC;
+    setSortField(field);
+    setSortDirection(direction);
 
-		const sortedArtworks = sortItems(artworks, field, direction);
-		setArtworks(sortedArtworks);
-	};
+    const sortedArtworks = sortItems(artworks, field, direction);
+    setArtworks(sortedArtworks);
+  };
 
-	return (
-		<div className={styles.artworksList}>
-			<table className={styles.table}>
-				<thead>
-					<tr>
-						<th onClick={() => handleSort('artwork_name')} />
-						<th
-							onClick={() => handleSort('artwork_name')}
-							className={classNames(styles.artworkTitle, {
-								[styles.active]: sortField === 'artwork_name',
-							})}
-						>
-							Artwork Name {renderSortIcon('artwork_name', sortField, sortDirection, ASC)}
-						</th>
-						<th
-							onClick={() => handleSort('artist_name')}
-							className={classNames(styles.artistTitle, {
-								[styles.active]: sortField === 'artist_name',
-							})}
-						>
-							Artist Name {renderSortIcon('artist_name', sortField, sortDirection, ASC)}
-						</th>
-						<th
-							onClick={() => handleSort('creation_year')}
-							className={classNames({
-								[styles.active]: sortField === 'creation_year',
-							})}
-						>
-							Years {renderSortIcon('creation_year', sortField, sortDirection, ASC)}
-						</th>
-						<th className={styles.headerTitle}>Status</th>
-						<th className={styles.headerTitle}>Visibility</th>
-						<th
-							onClick={() => handleSort('price')}
-							className={classNames({
-								[styles.active]: sortField === 'price',
-							})}
-						>
-							Price {renderSortIcon('price', sortField, sortDirection, ASC)}
-						</th>
-						<th className={styles.headerTitle}>Docs</th>
-						<th
-							onClick={() => handleSort('date_created')}
-							className={classNames({
-								[styles.active]: sortField === 'date_created',
-							})}
-						>
-							Updated {renderSortIcon('date_created')}
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					{artworks.map((artwork) => (
-						<>
-							<tr key={artwork.id}>
-								<td>
-									<picture>
-										{renderPictureSources(artwork.main_photo.transformations)}
-										<img
-											src={`images/${artwork.main_photo.filename_disk}`}
-											alt={artwork.main_photo.title}
-											className={styles.image}
-										/>
-									</picture>
-								</td>
-								<td className={styles.artworkTitle}>
-									{artwork.artwork_name} {artwork.has_notifications && <span className={styles.notificationDot}></span>}
-								</td>
-								<td className={styles.artistTitle}>{artwork.artist_name}</td>
-								<td>{artwork.creation_year}</td>
-								<td>{renderStatus(artwork.status)}</td>
-								<td>{renderVisibility(artwork.visibility)}</td>
-								<td>{`$ ${artwork.price.toLocaleString('en-US')}`}</td>
-								<td>{renderDocuments(artwork.documents_number)}</td>
-								<td>{dayjs(artwork.date_created).format('DD.MM.YYYY')}</td>
-							</tr>
-							<div className={styles.highlight} />
-						</>
-					))}
-				</tbody>
-			</table>
-		</div>
-	);
+  return (
+    <div className={styles.artworksList}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th onClick={() => handleSort("artwork_name")} />
+            <th
+              onClick={() => handleSort("artwork_name")}
+              className={classNames(styles.artworkTitle, {
+                [styles.active]: sortField === "artwork_name",
+              })}
+            >
+              Artwork Name{" "}
+              {renderSortIcon("artwork_name", sortField, sortDirection, ASC)}
+            </th>
+            <th
+              onClick={() => handleSort("artist_name")}
+              className={classNames(styles.artistTitle, {
+                [styles.active]: sortField === "artist_name",
+              })}
+            >
+              Artist Name{" "}
+              {renderSortIcon("artist_name", sortField, sortDirection, ASC)}
+            </th>
+            <th
+              onClick={() => handleSort("creation_year")}
+              className={classNames({
+                [styles.active]: sortField === "creation_year",
+              })}
+            >
+              Years{" "}
+              {renderSortIcon("creation_year", sortField, sortDirection, ASC)}
+            </th>
+            <th className={styles.headerTitle}>Status</th>
+            <th className={styles.headerTitle}>Visibility</th>
+            <th
+              onClick={() => handleSort("price")}
+              className={classNames({
+                [styles.active]: sortField === "price",
+              })}
+            >
+              Price {renderSortIcon("price", sortField, sortDirection, ASC)}
+            </th>
+            <th className={styles.headerTitle}>Docs</th>
+            <th
+              onClick={() => handleSort("date_created")}
+              className={classNames({
+                [styles.active]: sortField === "date_created",
+              })}
+            >
+              Updated {renderSortIcon("date_created")}
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {artworks.map((artwork) => (
+            <Fragment key={artwork.id}>
+              <tr>
+                <td>
+                  <picture>
+                    {renderPictureSources(artwork.main_photo.transformations)}
+                    <img
+                      src={`images/${artwork.main_photo.filename_disk}`}
+                      alt={artwork.main_photo.title}
+                      className={styles.image}
+                    />
+                  </picture>
+                </td>
+                <td className={styles.artworkTitle}>
+                  {artwork.artwork_name}{" "}
+                  {artwork.has_notifications && (
+                    <span className={styles.notificationDot}></span>
+                  )}
+                </td>
+                <td className={styles.artistTitle}>{artwork.artist_name}</td>
+                <td>{artwork.creation_year}</td>
+                <td>{renderStatus(artwork.status)}</td>
+                <td>{renderVisibility(artwork.visibility)}</td>
+                <td>{`$ ${artwork.price.toLocaleString("en-US")}`}</td>
+                <td>{renderDocuments(artwork.documents_number)}</td>
+                <td>{dayjs(artwork.date_created).format("DD.MM.YYYY")}</td>
+              </tr>
+              <div className={styles.highlight} />
+            </Fragment>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default ArtworksList;
